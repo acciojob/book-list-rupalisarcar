34051b56462517374885820b2fd4f883a43fd4d1
rupalisarcar <rupalisarcar@gmail.com>
2025-11-05 22:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,21 +14,25 @@ function addBook(event){
 	secCol.innerText=bookAuthor;
 	const thirdCol = document.createElement("td");
 	thirdCol.innerText=isbn;
-	
+
+	const rmvCol = document.createElement("td");
 	const removeBtn = document.createElement("button");
 	removeBtn.innerHTML = "&times;";
-
-	console.log(`firstCol= ${booktitle} secCol= ${bookAuthor}, thirdCol= ${isbn}`)
-	tableRow.appendChild(firstCol);
-	tableRow.appendChild(secCol);
-	tableRow.appendChild(thirdCol);
-	tableRow.appendChild(removeBtn);
-	
 	// Add some styling for better presentation (e.g., cursor pointer)
 	removeBtn.style.cursor = "pointer";
 	removeBtn.style.marginLeft = "10px"; // Optional styling
 	removeBtn.style.backgroundColor="red";
 	removeBtn.style.color="white";
+	removeBtn.style.border="none";
+
+	rmvCol.appendChild(removeBtn)
+	
+	tableRow.appendChild(firstCol);
+	tableRow.appendChild(secCol);
+	tableRow.appendChild(thirdCol);
+	tableRow.appendChild(rmvCol);
+	
+	
 
 	bookList.appendChild(tableRow)
 	removeBtn.addEventListener("click",removeBookList)	
